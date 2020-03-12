e9922b8262df9a3b489a4b702b5800a1c2049d3d
Kyle G <kylegilde@gmail.com>
2020-03-11 22:48:01 -0500

    update

--- util_functions.py
+++ util_functions.py
@@ -18,23 +18,4 @@ def get_object_name(obj):
     namespace = dict(globals(), **locals()) # inspect.stack()[1][0].f_globals
     return [name for name in namespace if namespace[name] is obj][0]
 
-def print_shape(x, return_shape=False):
-    """
-    Print the variable name and shape
 
-    Parameters
-    ----------
-    x: any variable with the attribute "shape"
-    return_shape: if True, returns the shape tuple, default is False
-
-    Returns
-    -------
-    Prints the object name and shape
-    Only returns the shape if return_shape=True
-    
-    """
-    x_name = get_object_name(x)
-    print(x_name, x.shape)
-    
-    if return_shape:
-        return x.shape
