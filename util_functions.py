aaa6001213036fa73d3dfda1dc8d7d691e0563b2
Kyle G <kylegilde@gmail.com>
2020-03-11 22:42:38 -0500

    bug fixed

--- util_functions.py
+++ util_functions.py
@@ -15,9 +15,8 @@ def get_object_name(obj):
     
     """
     import inspect
-    namespace = inspect.stack()[1][0].f_globals
+    namespace = dict(globals(), **locals()) # inspect.stack()[1][0].f_globals
     return [name for name in namespace if namespace[name] is obj][0]
-
 
 def print_shape(x, return_shape=False):
     """
@@ -34,8 +33,6 @@ def print_shape(x, return_shape=False):
     Only returns the shape if return_shape=True
     
     """
-    assert hasattr(x, 'shape'), 'Object does not have shape attribute'
-    
     x_name = get_object_name(x)
     print(x_name, x.shape)
     
